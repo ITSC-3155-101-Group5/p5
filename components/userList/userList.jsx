d4d555db55d8b3f41fa7550cadbc804e8bd14ad6
jlienghot <jlienghot@outlook.com>
2026-03-25 21:23:16 -0400

    Reapply "fixed components lil bit"
    This reverts commit 59ac45cbe8c0be66dd6928b01b343456dd6cd55e.

--- components/userList/userList.jsx
+++ components/userList/userList.jsx
@@ -8,9 +8,6 @@ from '@mui/material';
 import './userList.css';
 import fetchModel from "../../lib/fetchModelData";
 
-/**
- * Define UserList, a React component of project #5
- */
 class UserList extends React.Component {
     constructor(props) {
         super(props);
@@ -26,9 +23,8 @@ class UserList extends React.Component {
 
     componentDidUpdate() {
         const new_user_id = this.props.match?.params.userId;
-        //console.log(new_user_id);
+
         const current_user_id = this.state.user_id;
-        //console.log(current_user_id);
         if (current_user_id  !== new_user_id){
             this.handleUserChange(new_user_id);
         }
