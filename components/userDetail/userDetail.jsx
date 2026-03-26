2ff48a5abe4d9ea65b20f7d0baeabd3a56a8498e
Joshua <jtorre44@charlotte.edu>
2026-03-25 22:03:50 -0400

    Commit

--- components/userDetail/userDetail.jsx
+++ components/userDetail/userDetail.jsx
@@ -2,26 +2,49 @@ import React from 'react';
 import {
   Typography
 } from '@mui/material';
+import { Link } from 'react-router-dom';
 import './userDetail.css';
-
 
 /**
  * Define UserDetail, a React component of project #5
  */
 class UserDetail extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
+    const userId = this.props.match.params.userId;
+    const user = window.models.userModel(userId);
+
+    if (!user) {
+      return (
+        <Typography variant="body1">
+          User not found.
+        </Typography>
+      );
+    }
+
     return (
-      <Typography variant="body1">
-        This should be the UserDetail view of the PhotoShare app. Since
-        it is invoked from React Router the params from the route will be
-        in property match. So this should show details of user:
-        {this.props.match.params.userId}. You can fetch the model for the
-        user from window.models.userModel(userId).
-      </Typography>
+      <div>
+        <Typography variant="h4" gutterBottom>
+          {user.first_name} {user.last_name}
+        </Typography>
+
+        <Typography variant="body1" gutterBottom>
+          <strong>Location:</strong> {user.location}
+        </Typography>
+
+        <Typography variant="body1" gutterBottom>
+          <strong>Occupation:</strong> {user.occupation}
+        </Typography>
+
+        <Typography variant="body1" gutterBottom>
+          <strong>Description:</strong> {user.description}
+        </Typography>
+
+        <Typography variant="body1" sx={{ marginTop: '16px' }}>
+          <Link to={`/photos/${user._id}`}>
+            View {user.first_name}&apos;s Photos
+          </Link>
+        </Typography>
+      </div>
     );
   }
 }
