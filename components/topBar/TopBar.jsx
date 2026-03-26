75988bea2e1531fb174e006fc79cdde8032d39fa
Elijah James <ejames37@charlotte.edu>
2026-03-25 22:13:55 -0400

    Update TopBar.jsx

--- components/topBar/TopBar.jsx
+++ components/topBar/TopBar.jsx
@@ -1,50 +1,86 @@
 import React from 'react';
-
 import {
   AppBar, Toolbar, Typography
 } from '@mui/material';
+import FetchModel from '../../lib/fetchModelData';
 import './TopBar.css';
 
 /**
- * Define TopBar, a React componment of project #5
+ * Define TopBar, a React component of project #5
  */
 class TopBar extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      context: 'Users',
+      version: '',
+    };
   }
 
-  getContext() {
+  componentDidMount() {
+    this.updateContext();
+    this.fetchVersion();
+    window.addEventListener('hashchange', this.updateContext);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('hashchange', this.updateContext);
+  }
+
+  fetchVersion = () => {
+    FetchModel('/test/info')
+      .then((res) => {
+        this.setState({ version: res.data.__v });
+      })
+      .catch((err) => console.error(err));
+  };
+
+  updateContext = () => {
     const hash = window.location.hash;
 
     const photosMatch = hash.match(/^#\/photos\/([^/]+)$/);
     if (photosMatch) {
-      const user = window.models.userModel(photosMatch[1]);
-      if (user) {
-        return `Photos of ${user.first_name} ${user.last_name}`;
-      }
+      const userId = photosMatch[1];
+
+      FetchModel(`/user/${userId}`)
+        .then((res) => {
+          const user = res.data;
+          this.setState({
+            context: `Photos of ${user.first_name} ${user.last_name}`
+          });
+        })
+        .catch((err) => console.error(err));
+      return;
     }
 
     const usersMatch = hash.match(/^#\/users\/([^/]+)$/);
     if (usersMatch) {
-      const user = window.models.userModel(usersMatch[1]);
-      if (user) {
-        return `Profile of ${user.first_name} ${user.last_name}`;
-      }
+      const userId = usersMatch[1];
+
+      FetchModel(`/user/${userId}`)
+        .then((res) => {
+          const user = res.data;
+          this.setState({
+            context: `Profile of ${user.first_name} ${user.last_name}`
+          });
+        })
+        .catch((err) => console.error(err));
+      return;
     }
 
-    return 'Users';
-  }
-
+    this.setState({ context: 'Users' });
+  };
 
   render() {
     return (
       <AppBar className="topbar-appBar" position="absolute">
         <Toolbar>
           <Typography variant="h5" color="inherit" style={{ flexGrow: 1 }}>
-              Chelsey Clinton
+            Elijah James
           </Typography>
           <Typography variant="h6" color="inherit">
-              {this.getContext()}
+            {this.state.context} {this.state.version && `(v${this.state.version})`}
           </Typography>
         </Toolbar>
       </AppBar>
